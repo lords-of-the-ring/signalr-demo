d4b3eac081444c5b2326363ab50157586b3b3b66
Georgi Kirilov <georgi.kirilov@smartit.bg>
2025-06-05 17:15:25 +0300

    Remove old stuff

--- Web/Auth/JwtConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Web.Auth;
-
-public static class JwtConstants
-{
-    public const string SigningKeySection = "JWT_KEY";
-    public const string Issuer = "signalr-demo";
-    public const string Audience = "signalr-demo";
-}
--- Web/Signalr/SignalrConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Web.Signalr;
-
-public static class SignalrConstants
-{
-    public const string AccessTokenQueryParameter = "access_token";
-    public const string HubsBasePrefix = "/hubs";
-}
